99838836967477fa07be387727b5e7a633fcecd9
Antoine Payet <antoine.payet@viacesi.fr>
2025-06-06 11:15:22 +0200

    Création des différents modules (microservices) : * account-service * config-service * customer-service * discovery-service * gateway-service

---
+++ customer-service/src/test/java/fr/socaf/bankaccountapp/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.socaf.bankaccountapp.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
