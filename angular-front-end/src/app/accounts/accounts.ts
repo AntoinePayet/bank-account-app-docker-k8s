d8b0530aa545c1d847420eb45f34f31e08ef5ca7
Antoine Payet <antoine.payet@viacesi.fr>
2025-06-16 17:30:15 +0200

    Création du frontend pour afficher les customers et accounts récupérés dans le backend

--- angular-front-end/src/app/accounts/accounts.ts
+++ angular-front-end/src/app/accounts/accounts.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-accounts',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './accounts.html',
   styleUrl: './accounts.css'
 })
-export class Accounts {
+export class Accounts implements OnInit {
+  accounts: any;
 
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit() {
+    this.http.get('http://localhost:8888/ACCOUNT-SERVICE/accounts')
+      .subscribe({
+        next: data => {
+          this.accounts = data;
+          this.cdr.detectChanges();
+        },
+        error: err => {
+          console.error(err);
+        }
+      })
+  }
 }
--- angular-front-end/src/app/customers/customers.ts
+++ angular-front-end/src/app/customers/customers.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-customers',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './customers.html',
   styleUrl: './customers.css'
 })
-export class Customers {
+export class Customers implements OnInit {
+  customers: any;
 
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit() {
+    this.http.get('http://localhost:8888/CUSTOMER-SERVICE/customers')
+      .subscribe({
+        next: data => {
+          this.customers = data;
+          this.cdr.detectChanges();
+        },
+        error: err => {
+          console.error(err);
+        }
+      })
+  }
 }
